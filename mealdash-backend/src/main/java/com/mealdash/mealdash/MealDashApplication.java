3b65730521a1a8374e72a26030ea3e1c0de688dd
saurabhjojare <saurabh.jojare@gmail.com>
2025-05-18 19:44:19 +0530

    feat: add Dockerfile and update backend config

--- mealdash-backend/src/main/java/com/mealdash/mealdash/MealDashApplication.java
+++ mealdash-backend/src/main/java/com/mealdash/mealdash/MealDashApplication.java
@@ -2,8 +2,10 @@ package com.mealdash.mealdash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class MealDashApplication {
 
 	public static void main(String[] args) {
