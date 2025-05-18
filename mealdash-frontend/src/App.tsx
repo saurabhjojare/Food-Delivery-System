0118db2af8e226eabdd7dbe3d43fc626694ff3f6
saurabhjojare <saurabh.jojare@gmail.com>
2025-05-18 17:59:35 +0530

    Set up backend and frontend project structure

---
+++ mealdash-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        Welcome to MealDash
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ mealdash-frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
